6937d4af704f0a6c791791959b67954aac52c061
bharath0616 <108422500+bharath0616@users.noreply.github.com>
2024-03-08 16:40:42 +0530

    jsx files

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+function App(){
+  return(  
+    <>
+      <button type='submit' onClick className='text-slate-800 bg-grey align-middle '> hey</button>
+      </> 
+    
+  
+  )
+}
+export default App;
