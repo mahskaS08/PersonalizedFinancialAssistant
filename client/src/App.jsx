b6a643068705856653d7dee4b604469202973c73
bharath0616 <108422500+bharath0616@users.noreply.github.com>
2024-03-17 08:08:03 +0530

    Revert "basic ui of website"
    This reverts commit c4191403853e987cb0b4996deb10191c9a2d3e16.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,14 @@
-import React from 'react';
+
+import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import First from './pages/First';
-import Second from './pages/Second';
-import Third from './pages/Third';
-import Home from './pages/Home';
-
-export default function App() {
-  return (  
-    <Router>
-    <Header/>
-      <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/first' element={<First />} />
-      <Route path='/second' element={<Second />} />
-      <Route path='/third' element={<Third />} />
-      </Routes>
-    </Router>
-  ) 
+function App(){
+  return(  
+    <>
+      <button type='submit' onClick className='text-slate-800 bg-grey align-middle '> hey</button>
+      </> 
+    
+  
+  )
 }
-
+export default App;
