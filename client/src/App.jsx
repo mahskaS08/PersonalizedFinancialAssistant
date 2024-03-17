279af0a0074fe1ef5e98af4e50a62d8dad140a55
bharath0616 <108422500+bharath0616@users.noreply.github.com>
2024-03-17 08:24:09 +0530

    Basic UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,22 @@
-
-import './App.css';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-function App(){
-  return(  
-    <>
-      <button type='submit' onClick className='text-slate-800 bg-grey align-middle '> hey</button>
-      </> 
-    
-  
-  )
+import First from './pages/First';
+import Second from './pages/Second';
+import Third from './pages/Third';
+import Home from './pages/Home';
+
+export default function App() {
+  return (  
+    <Router>
+    <Header/>
+      <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/first' element={<First />} />
+      <Route path='/second' element={<Second />} />
+      <Route path='/third' element={<Third />} />
+      </Routes>
+    </Router>
+  ) 
 }
-export default App;
+
